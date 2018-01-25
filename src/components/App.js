3691a1d7ad4f74ac75a719988f0da95af49ecb29
Filipe Valente Gomes <filipego@Filipes-MBP.fios-router.home>
2018-01-24 23:46:07 -0500

    added elements and made it responsive

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,64 @@
-import React, { Component } from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import React, { Component, Fragment } from 'react'
 
-import Main from './Main'
-import HowLong from './HowLong'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+			shown: true,
+		};
+  }
+
+  toggle() {
+		this.setState({
+			shown: !this.state.shown
+		});
+	}
+
+
   render() {
+
+    var show = {
+			display: this.state.shown ? "block" : "none"
+		};
+
+		var hidden = {
+			display: this.state.shown ? "none" : "block"
+		}
+
     return (
-      <BrowserRouter>
-        <div className="container">
-          <Switch>
-            <Route exact path="/" component={Main} />
-            <Route path="/how-long" component={HowLong}/>
-          </Switch>
+      <Fragment>
+        <div className="container row">
+          <div className="main col">
+            <h2>How long is the drive?</h2>
+            <div className="content col">
+              <form>
+                <div className="col">
+                  <label>Origin:</label>
+                  <input type="text" name="IP1" value="" style={ show } />
+                  <div style={ hidden }>
+                    <p><span>1 West</span>New York, NY 10011</p>
+                  </div>
+                </div>
+                <div className="col">
+                  <label>Destination:</label>
+                  <input type="text" name="IP2" value="" style={ show } />
+                  <div style={ hidden }>
+                    <p><span>10 Bowery</span>New York, NY 10011</p>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
-      </BrowserRouter>
+        <div className="button-result row">
+          <button style={ show } className="btn" onClick={this.toggle.bind(this)}>Gimme the Distance!</button>
+          <div className="result" style={ hidden }>
+            result
+          </div>
+        </div>
+      </Fragment>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import './css/reset.css'
 import './scss/styles.css'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
