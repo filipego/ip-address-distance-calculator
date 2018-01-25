09e92e9173f4315f98557a1f7418583617c72d66
Filipe Valente Gomes <filipego@Filipes-MBP.fios-router.home>
2018-01-25 00:24:57 -0500

    fixed small css bug

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './css/reset.css'
 import './scss/styles.css'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
